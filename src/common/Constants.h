43c33d6ffd8ef014ef15571d7fc739cbe0133745
ABDUL <mateuszszczepanowski@gmail.com>
2022-06-05 05:13:21 -0700

    Dzialajaca wersja - miejmy nadzieje :)

--- src/common/Constants.h
+++ src/common/Constants.h
@@ -7,4 +7,6 @@ const int MAX_VALUE_SIZE = 32;
 const int PID_MAX_DIGITS = 5;
 const int MESSAGE_SIZE = (MAX_N_OF_ELEMENTS*(MAX_VALUE_SIZE+1) + 1 + 11);
 
+const int MAX_TIMEOUT_LENGTH = 3;
+
 #endif
--- src/common/TuplePatternMessage.h
+++ src/common/TuplePatternMessage.h
@@ -4,6 +4,7 @@
 #include "Enums.h"
 #include "Message.h"
 #include "TuplePattern.h"
+#include <string.h>
 
 class TuplePatternMessage : public Message {
 private:
@@ -24,6 +25,9 @@ public:
         serialized += std::to_string(this->getType());
         serialized += std::to_string(this->getCommand());
         serialized += std::to_string(this->getTimeout());
+        for (int i=0; i < (MAX_TIMEOUT_LENGTH - (std::to_string(this->getTimeout())).length()); i++) {
+            serialized += " ";
+        }
         serialized += this->pattern.serialize();
         return serialized;
     };
@@ -31,9 +35,9 @@ public:
         this->setPid( atoi(serialized.substr(0, PID_MAX_DIGITS).c_str()));
         this->setType( static_cast<MessageType>(serialized[PID_MAX_DIGITS]-48));
         this->setCommand( static_cast<Command>(serialized[PID_MAX_DIGITS+1]-48));
-        this->setTimeout( static_cast<int>(serialized[PID_MAX_DIGITS+2])-48);
+        this->setTimeout( atoi(serialized.substr(PID_MAX_DIGITS+2, MAX_TIMEOUT_LENGTH).c_str()) );
         // Wzorzec zajmuje MAX_N_OF_ELEMENTS*(MAX_VALUE_SIZE+1) + 1 bajty
-        this->pattern.deserialize(serialized.substr(PID_MAX_DIGITS+2, MAX_N_OF_ELEMENTS*(MAX_VALUE_SIZE+1) + 1).c_str());
+        this->pattern.deserialize(serialized.substr(PID_MAX_DIGITS+2+MAX_TIMEOUT_LENGTH, MAX_N_OF_ELEMENTS*(MAX_VALUE_SIZE+1) + 1).c_str());
     };
 };
 
